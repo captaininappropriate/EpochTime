9194e4bc813e30f2490cb9d9d38ed30ff9b9a8a7
consultant <consultant@kali.underground.com>
2021-11-08 18:21:32 +1100

    basic functionality for what i need right now

---
+++ EpochTime.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import time
+
+# get the current time 
+current = int(time.time())
+print(current)
+
+# add 15 minutes (900) seconds to the current time
+future = int(time.time() + 900)
+print(future)
